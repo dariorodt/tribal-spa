bb7c13bdeb57c328d61529f30e844b6f1fb49807
Dario Rodriguez <dariorodt@gmail.com>
2021-07-18 13:40:51 -0400

    Initial commit

---
+++ src/views/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <pre>{{ user }}</pre>
+    <form @submit.prevent="login">
+      <input v-model="form.email" type="email" placeholder="Email" /> <br />
+      <input v-model="form.password" type="password" placeholder="Password" />
+      <br />
+      <button>Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    user: {},
+    form: {
+      email: "",
+      password: "",
+    },
+  }),
+  methods: {
+    login() {
+      console.log("Enviando el login", this.form);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
